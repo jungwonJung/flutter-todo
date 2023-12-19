f4e59c941fbfc219a4583018a4c32e1bb3672bd4
jungwonJung <wjdwjd1501@gmail.com>
2023-12-19 17:53:58 +0100

    add feature create todo and delete todo

--- lib/main.dart
+++ lib/main.dart
@@ -53,15 +53,26 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
+class TodoItem {
+  String title;
+  bool isChecked;
+
+  TodoItem({required this.title, this.isChecked = false});
+}
+
 class _MainPageState extends State<MainPage> {
   late String dow;
   late String month;
   late String day;
+  late TextEditingController _todoController;
+
+  List<TodoItem> todoList = [];
 
   @override
   void initState() {
     super.initState();
     updateDay();
+    _todoController = TextEditingController();
   }
 
   void updateDay() {
@@ -70,6 +81,12 @@ class _MainPageState extends State<MainPage> {
       month = DateFormat('MMMM').format(DateTime.now());
       day = DateFormat('dd').format(DateTime.now());
     });
+  }
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
   }
 
   @override
@@ -102,11 +119,17 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ],
               ),
-              const Icon(
-                Icons.settings,
-                size: 40,
-                color: Colors.blueGrey,
-              )
+              if (todoList.isNotEmpty) // 리스트가 비어있지 않을 때만 아이콘 표시
+                IconButton(
+                  onPressed: () {
+                    _showDeleteDialog(context);
+                  },
+                  icon: const Icon(
+                    Icons.settings,
+                    size: 40,
+                    color: Colors.blueGrey,
+                  ),
+                ),
             ],
           ),
         ),
@@ -158,11 +181,50 @@ class _MainPageState extends State<MainPage> {
                     ),
                   ),
                   Positioned(
+                    left: 16.0,
+                    top: 16.0,
+                    right: 16.0,
+                    bottom: 100.0,
+                    child: ListView.builder(
+                      itemCount: todoList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: ListTile(
+                            title: Text(todoList[index].title),
+                            leading: Checkbox(
+                              value: todoList[index].isChecked,
+                              onChanged: (value) {
+                                setState(() {
+                                  todoList[index].isChecked = value ?? false;
+                                });
+                              },
+                              activeColor: Colors.blue,
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith<Color>(
+                                (Set<MaterialState> states) {
+                                  if (states.contains(MaterialState.selected)) {
+                                    return Colors.blue.withOpacity(0.85);
+                                  }
+
+                                  return Colors.blue.withOpacity(0.2);
+                                },
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Positioned(
                     right: 16.0,
                     bottom: 16.0,
                     child: FloatingActionButton(
                       backgroundColor: Colors.orange,
-                      onPressed: () {},
+                      onPressed: () {
+                        _showAddTodoDialog(context);
+                      },
                       shape: const CircleBorder(),
                       child: const Icon(
                         Icons.create_sharp,
@@ -177,6 +239,94 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> _showAddTodoDialog(BuildContext context) async {
+    String newTodo = '';
+    _todoController.clear(); // 추가: TextField 초기화
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Add TODO',
+            style: TextStyle(color: Colors.blue),
+          ),
+          content: TextField(
+            controller: _todoController,
+            onChanged: (value) {
+              newTodo = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Enter your TODO',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_todoController.text.isNotEmpty) {
+                  setState(() {
+                    todoList.add(TodoItem(title: newTodo));
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showDeleteDialog(BuildContext context) async {
+    if (todoList.isEmpty) return;
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete All TODOs'),
+          content: const Text('Are you sure you want to delete all TODOs?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  todoList.clear();
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Delete',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
